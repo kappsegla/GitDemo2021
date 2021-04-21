54f0b8fc0193eee91b6f0a8ea993b29dd464eb5b
Martin Blomberg <martin.blomberg@outlook.com>
2021-04-21 14:34:15 +0200

    Refactored printing of hello

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
 
 Console.WriteLine("Hello World!");
-Console.WriteLine("Feature1 Branch says hello!");
-Console.WriteLine("Feature2 Branch says hello!");
+Console.WriteLine("Feature1 and Feature2 Branches says hello!");
